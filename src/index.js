ac299653297ac566361b6dd5bc7cd968923bf67d
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-03-30 22:53:23 +0400

    Bookstore Lib

--- src/index.js
+++ src/index.js
@@ -7,11 +7,58 @@
  */
 class HalloweenCampaign
 {   
-    
-    constructor() 
+    /**
+     * Constructor
+     * @param {*} books 
+     * @param {*} userIsVIP 
+     */
+    constructor(books, userIsVIP) 
     {
-        //
+        this.books = books;
+        this.cart  = [];
+        this.userIsVIP = userIsVIP;
     }
+
+    /**
+     * Add item.id to cart
+     * @param {*} id 
+     * 
+     * @returns void
+     */
+    addToCart(id)
+    {
+        return (this.cart).push(this.books[id]);
+    }
+
+
+    /**
+     * Calculates Total Price
+     * 
+     * @returns total price
+     */
+    totalPrice()
+    {
+        let totalPrice = 0;
+
+        for(let i=0; i < (this.books).length; i++) {
+            totalPrice += this.books[i].price
+        }
+
+        return this.applyDiscount(totalPrice);
+    }
+
+
+    /**
+     * Calculates Discount
+     * @param {*} totalPrice 
+     * 
+     * @returns discounted price
+     */
+    applyDiscount(totalPrice)
+    {
+        return totalPrice;
+    }
+
 }
 
 module.exports = HalloweenCampaign;
