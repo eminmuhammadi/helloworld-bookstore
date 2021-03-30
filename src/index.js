c8f6f1ad11d85e6c4aef0e9247e23f011d67afd7
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-03-30 18:37:17 +0400

    Boilerplate Generated

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/**
+ * HalloweenCampaign
+ * by Hello World Bookstore
+ * 
+ * @author Emin Muhammadi
+ * @version 0.1.0
+ */
+class HalloweenCampaign
+{   
+    
+    constructor() 
+    {
+        //
+    }
+}
+
+module.exports = HalloweenCampaign;
